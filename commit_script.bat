c418269cd4a102d4a48a0a652c6030a36087f0ba
d-guo <dguo@ucsb.edu>
2019-12-23 19:10:50 -0800

    added commit_file

--- commit_script.bat
+++ commit_script.bat
@@ -1,3 +1,3 @@
 set /A n=%RANDOM% %% 15 + 10
 
-python3 commit_script.py %n%
+py commit_script.py %n%
--- commit_script.py
+++ commit_script.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-n = sys.argv[1]
+n = int(sys.argv[1])
 
 for i in range(n):
     if(os.path.isfile('commit_file')):
@@ -10,7 +10,7 @@ for i in range(n):
         os.system('git commit -m "deleted commit_file')
         os.system('git push')
     else:
-        open('commit_file', 'r').close()
+        open('commit_file', 'w+').close()
         os.system('git add .')
         os.system('git commit -m "added commit_file')
         os.system('git push')
